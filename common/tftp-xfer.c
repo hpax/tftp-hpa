b4b8442a7a64a4f8cc2955a83cf2de71627fdaee
H. Peter Anvin <hpa@zytor.com>
2026-08-29 16:09:21 -0700

    common: add shared TFTP transfer engine
    Prepare to merge the data transfer code between client and server.
    
    Signed-off-by: H. Peter Anvin <hpa@zytor.com>

---
+++ common/tftp-xfer.c
@@ -0,0 +1,249 @@
+/*
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Copyright (c) 2026 H. Peter Anvin
+ */
+
+#include "tftp-xfer.h"
+
+static void set_result(struct tftp_xfer_result *result,
+                       enum tftp_xfer_status status, int io_result,
+                       int error, uintmax_t bytes)
+{
+    result->status = status;
+    result->io_result = io_result;
+    result->error = error;
+    result->bytes = bytes;
+}
+
+static void finish(const struct tftp_xfer *xfer,
+                   struct tftp_xfer_result *result,
+                   enum tftp_xfer_status status, int io_result, int error,
+                   uintmax_t bytes)
+{
+    xfer->ops->retry_leave(xfer->context);
+    set_result(result, status, io_result, error, bytes);
+}
+
+static uint16_t next_block(uint16_t block, uint16_t rollover)
+{
+    if (!++block)
+        block = rollover;
+    return block;
+}
+
+void tftp_xfer_send(const struct tftp_xfer *xfer,
+                    struct tftp_xfer_result *result)
+{
+    struct tftphdr *dp;
+    struct tftphdr *ap;
+    char *packets;
+    int *lengths;
+    size_t packetsize;
+    sigjmp_buf retrybuf;
+    volatile uint16_t block = 1;
+    volatile uintmax_t bytes = 0;
+    volatile int packet_count;
+    volatile int final;
+    int n;
+    int size;
+    int restarted;
+    uint16_t opcode;
+    uint16_t packet_block;
+    uint16_t expected_ack;
+
+    packetsize = ((size_t)xfer->blocksize + 5) & ~(size_t)1;
+    packets = xcalloc(xfer->windowsize, packetsize);
+    lengths = xcalloc(xfer->windowsize, sizeof(*lengths));
+    dp = r_init();
+
+    for (;;) {
+        packet_count = 0;
+        final = 0;
+        do {
+            size = readit(xfer->file, &dp, xfer->convert);
+            if (size < 0) {
+                finish(xfer, result, TFTP_XFER_READ_ERROR, size, errno,
+                       bytes);
+                goto out;
+            }
+            dp->th_opcode = htons((uint16_t)DATA);
+            dp->th_block = htons(block);
+            memcpy(packets + (size_t)packet_count * packetsize, dp,
+                   (size_t)size + 4);
+            lengths[packet_count++] = size + 4;
+            read_ahead(xfer->file, xfer->convert);
+            if (size != (int)xfer->blocksize)
+                final = 1;
+            block = next_block(block, xfer->rollover);
+        } while (packet_count < (int)xfer->windowsize && !final);
+
+        restarted = sigsetjmp(retrybuf, 1);
+        xfer->ops->retry_enter(xfer->context, retrybuf, restarted);
+      resend_window:
+        for (n = 0; n < packet_count; n++) {
+            dp = (struct tftphdr *)(packets + (size_t)n * packetsize);
+            if (xfer->ops->send(xfer->context, dp, lengths[n]) < 0) {
+                finish(xfer, result, TFTP_XFER_SEND_ERROR, -1, errno,
+                       bytes);
+                goto out;
+            }
+        }
+
+        xfer->ops->wait_begin(xfer->context);
+        for (;;) {
+            n = xfer->ops->recv(xfer->context, xfer->control,
+                                xfer->control_size);
+            if (n < 0) {
+                finish(xfer, result, TFTP_XFER_RECV_ERROR, n, errno,
+                       bytes);
+                goto out;
+            }
+            if (n < 4)
+                continue;
+            ap = xfer->control;
+            xfer->ops->received(xfer->context, ap, n);
+            opcode = ntohs(ap->th_opcode);
+            packet_block = ntohs(ap->th_block);
+            if (opcode == ERROR) {
+                finish(xfer, result, TFTP_XFER_PEER_ERROR, n, 0, bytes);
+                goto out;
+            }
+            expected_ack = ntohs(((struct tftphdr *)
+                                  (packets + (size_t)(packet_count - 1) *
+                                   packetsize))->th_block);
+            if (opcode == ACK && packet_block == expected_ack)
+                break;
+            if (opcode == OACK && xfer->resend_oack)
+                goto resend_window;
+            if (opcode == ACK)
+                xfer->ops->drain(xfer->context);
+        }
+        for (n = 0; n < packet_count; n++)
+            bytes += lengths[n] - 4;
+        if (final) {
+            finish(xfer, result, TFTP_XFER_OK, 0, 0, bytes);
+            goto out;
+        }
+    }
+
+  out:
+    xfree(lengths);
+    xfree(packets);
+}
+
+void tftp_xfer_recv(const struct tftp_xfer *xfer, struct tftphdr *ack,
+                    const struct tftphdr *initial_reply,
+                    int initial_reply_len, struct tftphdr *initial_packet,
+                    int initial_packet_len, struct tftp_xfer_result *result)
+{
+    struct tftphdr *dp;
+    const struct tftphdr *reply;
+    sigjmp_buf retrybuf;
+    volatile uint16_t block = 1;
+    volatile uint16_t last_acked = 0;
+    uint16_t opcode;
+    volatile uint16_t packet_block;
+    volatile uintmax_t bytes = 0;
+    int packets_in_window = 0;
+    int initial_reply_pending = initial_reply != NULL;
+    volatile int initial_packet_pending = initial_packet != NULL;
+    int reply_len;
+    int n;
+    int size;
+    int final;
+    int restarted;
+
+    dp = w_init();
+    if (initial_packet_pending)
+        dp = initial_packet;
+
+    for (;;) {
+        restarted = sigsetjmp(retrybuf, 1);
+        xfer->ops->retry_enter(xfer->context, retrybuf, restarted);
+        if (initial_reply_pending || restarted) {
+            if (initial_reply_pending) {
+                reply = initial_reply;
+                reply_len = initial_reply_len;
+            } else {
+                ack->th_opcode = htons((uint16_t)ACK);
+                ack->th_block = htons(last_acked);
+                reply = ack;
+                reply_len = 4;
+            }
+            if (xfer->ops->send(xfer->context, reply, reply_len) < 0) {
+                finish(xfer, result, TFTP_XFER_SEND_ERROR, -1, errno,
+                       bytes);
+                return;
+            }
+            xfer->ops->flush(xfer->context);
+        }
+
+        if (initial_packet_pending) {
+            n = initial_packet_len;
+            initial_packet_pending = 0;
+        } else {
+            xfer->ops->wait_begin(xfer->context);
+            for (;;) {
+                n = xfer->ops->recv(xfer->context, dp,
+                                    MAX_SEGSIZE + 4);
+                if (n < 0) {
+                    finish(xfer, result, TFTP_XFER_RECV_ERROR, n, errno,
+                           bytes);
+                    return;
+                }
+                if (n < 4)
+                    continue;
+                xfer->ops->received(xfer->context, dp, n);
+                opcode = ntohs(dp->th_opcode);
+                packet_block = ntohs(dp->th_block);
+                if (opcode == ERROR) {
+                    finish(xfer, result, TFTP_XFER_PEER_ERROR, n, 0,
+                           bytes);
+                    return;
+                }
+                if (opcode != DATA)
+                    continue;
+                if (packet_block == block)
+                    break;
+                if (packet_block == last_acked) {
+                    ack->th_opcode = htons((uint16_t)ACK);
+                    ack->th_block = htons(last_acked);
+                    (void)xfer->ops->send(xfer->context, ack, 4);
+                }
+            }
+        }
+
+        initial_reply_pending = 0;
+        if (n - 4 > (int)xfer->blocksize) {
+            finish(xfer, result, TFTP_XFER_BAD_DATA, n, 0, bytes);
+            return;
+        }
+        size = writeit(xfer->file, &dp, n - 4, xfer->convert);
+        if (size != n - 4) {
+            finish(xfer, result, TFTP_XFER_WRITE_ERROR, size, errno,
+                   bytes);
+            return;
+        }
+        bytes += size;
+        final = size != (int)xfer->blocksize;
+        packets_in_window++;
+        block = next_block(block, xfer->rollover);
+        if (final || packets_in_window == (int)xfer->windowsize) {
+            last_acked = packet_block;
+            packets_in_window = 0;
+            ack->th_opcode = htons((uint16_t)ACK);
+            ack->th_block = htons(last_acked);
+            if (xfer->ops->send(xfer->context, ack, 4) < 0) {
+                finish(xfer, result, TFTP_XFER_SEND_ERROR, -1, errno,
+                       bytes);
+                return;
+            }
+            xfer->ops->flush(xfer->context);
+            if (final) {
+                finish(xfer, result, TFTP_XFER_OK, 0, 0, bytes);
+                return;
+            }
+        }
+    }
+}
---
+++ common/tftp-xfer.h
@@ -0,0 +1,75 @@
+/*
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Copyright (c) 2026 H. Peter Anvin
+ */
+
+#ifndef TFTP_XFER_H
+#define TFTP_XFER_H
+
+#include "tftpsubs.h"
+
+enum tftp_xfer_status {
+    TFTP_XFER_OK,
+    TFTP_XFER_SEND_ERROR,
+    TFTP_XFER_RECV_ERROR,
+    TFTP_XFER_READ_ERROR,
+    TFTP_XFER_WRITE_ERROR,
+    TFTP_XFER_BAD_DATA,
+    TFTP_XFER_PEER_ERROR
+};
+
+struct tftp_xfer_result {
+    enum tftp_xfer_status status;
+    int io_result;
+    int error;
+    uintmax_t bytes;
+};
+
+/*
+ * The transport callbacks keep packet I/O, timeout implementation, tracing,
+ * and error reporting local to the client or daemon.  recv() is responsible
+ * for its own timeout mechanism.  retry_enter() supplies its jump buffer to
+ * that mechanism, and is called again after it transfers control there.
+ */
+struct tftp_xfer_ops {
+    int (*send)(void *, const void *, int);
+    int (*recv)(void *, void *, int);
+    void (*received)(void *, const struct tftphdr *, int);
+    void (*drain)(void *);
+    void (*retry_enter)(void *, sigjmp_buf, int);
+    void (*retry_leave)(void *);
+    void (*wait_begin)(void *);
+    void (*flush)(void *);
+};
+
+struct tftp_xfer {
+    FILE *file;
+    int convert;
+    unsigned int blocksize;
+    unsigned int windowsize;
+    uint16_t rollover;
+    int resend_oack;
+    void *control;
+    int control_size;
+    void *context;
+    const struct tftp_xfer_ops *ops;
+};
+
+/*
+ * Send a file after any request or OACK exchange is complete.
+ */
+void tftp_xfer_send(const struct tftp_xfer *, struct tftp_xfer_result *);
+
+/*
+ * Receive a file after any request or OACK exchange is complete.  ack is
+ * the reusable ACK buffer.  initial_reply, when supplied, is sent before
+ * waiting for DATA 1 (typically OACK or ACK 0).  initial_packet is DATA 1
+ * that the request exchange has already received.
+ */
+void tftp_xfer_recv(const struct tftp_xfer *, struct tftphdr *ack,
+                    const struct tftphdr *initial_reply,
+                    int initial_reply_len, struct tftphdr *initial_packet,
+                    int initial_packet_len, struct tftp_xfer_result *);
+
+#endif /* TFTP_XFER_H */
