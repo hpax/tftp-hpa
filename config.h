bdf6b54217739597fb5fb76aa9b9e0ac2446f0f8
H. Peter Anvin <hpa@zytor.com>
2026-09-20 22:18:01 -0700

    config.h: fix portability issues with TYPE_MIN() and TYPE_MAX()
    Jump through hoops to avoid nonportable behavior in TYPE_MIN() and
    TYPE_MAX():
    
    1. Use TYPE_MAX() as the base, not TYPE_MIN(), thus avoiding negative
       numbers.
    2. Construct the mask in two parts to not set the type sign bit.
    3. Use ~ in TYPE_MIN() to invert the mask.
    
    Signed-off-by: H. Peter Anvin <hpa@zytor.com>

--- config.h
+++ config.h
@@ -309,9 +309,10 @@ static inline void *mempcpy(void *dst, const void *src, size_t n)
 #endif
 
 /* The max and min values of an integer type (assumes 2's complement) */
-#define IS_SIGNED(t)	((t)(-1) < (t)0)
-#define TYPE_MIN(t)	((t)((t)IS_SIGNED(t) << (sizeof(t)*CHAR_BIT - 1)))
-#define TYPE_MAX(t)	((t)(TYPE_MIN(t) - 1))
+#define IS_SIGNED(t)	 ((t)(-1) < (t)0)
+#define TYPE_MKMASK(t,x) ((t)(x) + ((t)(x) - 1))
+#define TYPE_MAX(t)	 TYPE_MKMASK(t, (t)1 << (sizeof(t)*CHAR_BIT-1-IS_SIGNED(t)))
+#define TYPE_MIN(t)	 ((t)~TYPE_MAX(t))
 
 /* off_t and OFF_T_MAX */
 #ifndef HAVE_OFF_T
